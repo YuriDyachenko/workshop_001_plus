bbdb52ca1a922c621c405c558fd7fc1e441ec9db
YuriDyachenko <yuri.dyachenko@outlook.com>
2021-02-02 18:07:30 +0300

    подключен объект Err, пока тупым копированием файла :(

---
+++ src/kurs/Err.java
@@ -0,0 +1,201 @@
+package kurs;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class Err {
+    public static final int PRINT_NONE = 0;
+    public static final int PRINT_MAIN = 1;
+    public static final int PRINT_FULL = 2;
+
+    //попробую внутри сделать создаваемый один раз объект
+    //чтобы можно было к нему обращаться, когда лень создавать свои
+    private static final Err last = new Err();
+
+    private boolean er = false;
+    private String msg = null;
+    private Exception e = null;
+
+    /*
+    стандартная проверка, есть ли ошибка
+    */
+    public boolean hasError() {
+        return er;
+    }
+
+    /*
+    проверка последней ошибки
+    */
+    public static boolean wasError() {
+        return last.hasError();
+    }
+
+    /*
+    проверка последней ошибки, а заодно и печать о ней, если задано
+    */
+    public static boolean wasError(int needPrint) {
+        boolean res = last.hasError();
+        if (needPrint == PRINT_MAIN || needPrint == PRINT_FULL) last.printMsg();
+        if (needPrint == PRINT_FULL) last.printStackTrace();
+        return res;
+    }
+
+    /*
+    остальные методы предлагается вызывать уже из полученного
+    этим методом объекта
+    */
+    public static Err getLast() {
+        return last;
+    }
+
+    /*
+    единый внутренний метод очистки
+    */
+    private void clearInner() {
+        er = false;
+        msg = null;
+        e = null;
+        //очистка любого объекта очищает и last
+        //но только если это не он сам, а то будет бесконечная рекурсия
+        if (this != last)
+            clear(last);
+    }
+
+    /*
+    единый внутренний метод, через который идет установка любой ошибки
+    */
+    private void setInner(String msg, Exception e) {
+        er = true;
+        if (msg == null && e != null) {
+            this.msg = e.getMessage();
+            //здесь тоже может так случиться, что запишется null
+            if (this.msg == null) {
+                //можно попробовать хоть что-то записать
+                //вот только забыл, как такое смоделировать, но вроде сталкивался где-то
+                this.msg = e.toString();
+            }
+        } else
+            if (msg != null)
+                this.msg = msg;
+        if (e != null)
+            this.e = e;
+        //установка любой ошибки заполняет и last
+        //но только если это не он сам, а то будет бесконечная рекурсия
+        if (this != last)
+            set(last, msg, e);
+    }
+
+    /*
+    сокращенный внутренний метод, вызывает единый
+    */
+    private void setInner(String msg) {
+        setInner(msg, null);
+    }
+
+    /*
+    возвращает пользовательское сообщение об ошибке
+    */
+    public String getMsg() {
+        if (msg == null) return "";
+        return msg;
+    }
+
+    /*
+    возвращает сообщение об ошибке из поля e
+    */
+    public String getDetailMsg() {
+        if (e == null) return "";
+        String s = e.getMessage();
+        if (s == null || s.isEmpty()) return "";
+        return s;
+    }
+
+    /*
+    возвращает трассировку из поля e
+    */
+    public String getStackTrace() {
+        if (e == null) return "";
+        StringWriter sw = new StringWriter();
+        e.printStackTrace(new PrintWriter(sw));
+        return sw.toString();
+    }
+
+    /*
+    печатает трассировку из поля e
+    */
+    public void printStackTrace() {
+        String s = getStackTrace();
+        if (s == null || s.isEmpty()) return;
+        System.out.println(s);
+    }
+
+    /*
+    печатает пользовательское сообщение об ошибке
+    */
+    public void printMsg() {
+        String s = getMsg();
+        if (s == null || s.isEmpty()) return;
+        System.out.println(s);
+    }
+
+    /*
+    печатает сообщение об ошибке из поля e
+    */
+    public void printDetailMsg() {
+        String s = getDetailMsg();
+        if (s == null || s.isEmpty()) return;
+        System.out.println(s);
+    }
+
+    /*
+    статический метод чистит переданную ошибку с проверкой на нулл
+    */
+    public static void clear(Err err) {
+        if (err == null) return;
+        err.clearInner();
+    }
+
+    /*
+    статический метод без параметров чистит именно последнюю ошибку
+    */
+    public static void clear() {
+        last.clearInner();
+    }
+
+    /*
+    статический метод без параметров назначает именно последнюю ошибку
+    */
+    public static void set(String msg, Exception e) {
+        last.setInner(msg, e);
+    }
+
+    /*
+    сокращенный статический метод без параметров назначает именно последнюю ошибку
+    */
+    public static void set(String msg) {
+        set(msg, null);
+    }
+
+    /*
+    статический самый кородкий метод
+    */
+    public static void set(Exception e) {
+        last.setInner(null, e);
+    }
+
+    /*
+    статический метод назначает именно ошибку
+    */
+    public static void set(Err err, String msg, Exception e) {
+        if (err == null)  return;
+        err.setInner(msg, e);
+    }
+
+    /*
+    сокращенный статический метод назначает именно ошибку
+    */
+    public static void set(Err err, String msg) {
+        set(err, msg, null);
+    }
+
+}
--- src/kurs/Main.java
+++ src/kurs/Main.java
@@ -12,63 +12,71 @@ package kurs;
 //С 17 часов до 23 часов включительно программа при запуске пишет - "Добрый вечер, {username}"
 //Само собой, {username} должен заменяться на Ваше имя.
 
-import org.json.simple.parser.JSONParser;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.util.Calendar;
 
 public class Main {
 
     public static void main(String[] args) {
-
         String userName = "Юрий";
+
         int hour = hourOfDay();
-        if (hour == -1) {
-            System.out.println("Ошибка при получении hourOfDay()...");
-            return;
-        }
+        //можно сразу выйти, потому что ошибка уже выведена на "своих местах"
+        if (hour == -1) return;
+
         String hello = hour > 17 ? "Добрый вечер" : hour > 10 ? "Добрый день" :
                 hour > 5 ? "Доброе утро" : "Доброй ночи";
         System.out.printf("%s, %s!\n", hello, userName);
-
     }
 
     private static int hourOfDay() {
-        String text;
-        try {
-            text = getDateStringFromInternet();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return -1;
-        }
+        String text = getDateStringFromInternet();
+        if (Err.wasError(Err.PRINT_FULL)) return -1;
 
-        Long unixTime;
-        try {
-            unixTime = extractUnixTimeFromJSON(text, "unixtime");
-            if (unixTime == null || unixTime == 0) return -1;
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return -1;
-        }
+        Long unixTime = extractUnixTimeFromJSON(text, "unixtime");
+        if (Err.wasError(Err.PRINT_FULL)) return -1;
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(unixTime * 1000);
         return calendar.get(Calendar.HOUR_OF_DAY);
     }
 
-    private static String getDateStringFromInternet() throws IOException {
-        final URL url = new URL("http://worldtimeapi.org/api/timezone/Etc/GMT-3");
-        final HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setConnectTimeout(500);
-        con.setReadTimeout(500);
+    private static String getDateStringFromInternet(){
+        Err.clear();
+
+        URL url;
+        try {
+            url = new URL("http://worldtimeapi.org/api/Etc/GMT-3");
+        } catch (MalformedURLException e) {
+            Err.set(e);
+            return null;
+        }
+
+        HttpURLConnection con;
+        try {
+            con = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            Err.set(e);
+            return null;
+        }
+
+        try {
+            con.setRequestMethod("GET");
+        } catch (ProtocolException e) {
+            Err.set(e);
+            return null;
+        }
+
         try (final BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
             String inputLine;
             final StringBuilder content = new StringBuilder();
@@ -76,15 +84,23 @@ public class Main {
                 content.append(inputLine);
             }
             return content.toString();
-        } catch (final Exception ex) {
-            ex.printStackTrace();
+        } catch (final Exception e) {
+            Err.set(e);
             return "";
         }
     }
 
-    public static Long extractUnixTimeFromJSON(String textJSON, String name) throws ParseException {
-        JSONObject jsonObject = (JSONObject) new JSONParser().parse(textJSON);
-        return (Long) jsonObject.get(name);
+    public static Long extractUnixTimeFromJSON(String textJSON, String name){
+        Err.clear();
+        try {
+            JSONObject jsonObject = (JSONObject) new JSONParser().parse(textJSON);
+            Long res = (Long) jsonObject.get(name);
+            if (res == null) Err.set("Нет такого поля: " + name);
+            return res;
+        } catch (ParseException e) {
+            Err.set(e);
+            return null;
+        }
     }
 
 }
